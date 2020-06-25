jkbremer/univention-corporate-server

commit 0c005a03f0c0063c1f68c7d7fcc52b1a4c4cc504

Bug #51246 ucslint: Ignore more DH files
<man:dh_systemd_enable(1)>
<man:dh_python2(1)>
<man:dh_python3(1)>

packaging/ucslint/univention/ucslint/0011-Control.py
@@ -190,8 +190,11 @@ class UniventionPackageCheck(uub.UniventionPackageCheckDebian):
 		'compat',  # dh
 		'control',
 		'copyright',  # dh_installdocs default
+		'debhelper-build-stamp',  # dh
 		'files',  # dh_builddeb
 		'NEWS',  # dh_installchangelogs default
+		'pydist-overrides',  # dh_python2
+		'py3dist-overrides',  # dh_python3
 		'rules',
 		'source.lintian-overrides',  # dh_lintian
 		'ucslint.overrides',
@@ -199,6 +202,7 @@ class UniventionPackageCheck(uub.UniventionPackageCheckDebian):
 
 	KNOWN_DH_FILES = set((
 		'bash-completion',  # dh_bash-completion
+		'bcep',  # dh_python3
 		'bug-control',  # dh_bugfiles
 		'bug-presubj',  # dh_bugfiles
 		'bug-script',  # dh_bugfiles
@@ -237,13 +241,23 @@ class UniventionPackageCheck(uub.UniventionPackageCheckDebian):
 		'prerm',  # dh_installdeb
 		'prerm.debhelper',  # dh_installdeb
 		'README.Debian',  # dh_installdocs
+		'mount',  # dh_systemd_enable
+		'path',  # dh_systemd_enable
+		'pydist',  # dh_python2 dh_python3
+		'pyinstall',  # dh_python2 dh_python3
+		'pyremove',  # dh_python2 dh_python3
+		'service',  # dh_systemd_enable
 		'sgmlcatalogs',  # dh_installcatalogs
 		'sharedmimeinfo',  # dh_installmime
 		'shlibs',  # dh_installdeb
+		'socket',  # dh_systemd_enable
 		'substvars',  # dh_gencontrol
 		'symbols',  # dh_makeshlibs
 		'symbols.i386',  # dh_makeshlibs
+		'target',  # dh_systemd_enable
 		'templates',  # dh_installdebconf
+		'timer',  # dh_systemd_enable
+		'tmpfile',  # dh_systemd_enable
 		'TODO',  # dh_installdocs
 		'triggers',  # dh_installdeb
 		'umc-modules',  # dh-umc-modules-install
@@ -251,9 +265,9 @@ class UniventionPackageCheck(uub.UniventionPackageCheckDebian):
 		'univention-config-registry-mapping',  # univention-install-config-registry-info
 		'univention-config-registry',  # univention-install-config-registry
 		'univention-config-registry-variables',  # univention-install-config-registry-info
+		'univention-l10n',  # univention-l10n-build / univention-l10n-install
 		'univention-service',  # univention-install-service-info
 		'wm',  # dh_installwm
-		'univention-l10n',  # univention-l10n-build / univention-l10n-install
 	))
 
 	NAMED_DH_FILES = set((

commit 7a3ce595400ed856e99be52afe29057c71560bf7

Bug #51246 ucslint: Ignore more debian/ files
<man:uscan(1)>
<man:dh_installinit(1)>

Fixes: 0c005a03f0c0063c1f68c7d7fcc52b1a4c4cc504

packaging/ucslint/univention/ucslint/0011-Control.py
@@ -193,6 +193,7 @@ class UniventionPackageCheck(uub.UniventionPackageCheckDebian):
 		'rules',
 		'source.lintian-overrides',  # dh_lintian
 		'ucslint.overrides',
+		'watch',  # uscan
 	))
 
 	KNOWN_DH_FILES = set((
@@ -277,6 +278,8 @@ class UniventionPackageCheck(uub.UniventionPackageCheckDebian):
 		'if-pre-up',  # dh_installifupdown
 		'if-up',  # dh_installifupdown
 		'init',  # dh_installinit
+		'init.d',  # dh_installinit
+		'isinstallable',  # Debian Installer
 		'logcheck.cracking',  # dh_installlogcheck
 		'logcheck.ignore.paranoid',  # dh_installlogcheck
 		'logcheck.ignore.server',  # dh_installlogcheck